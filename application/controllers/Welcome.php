b78038b34b57fce6bbda1aae2326bead45bad15a
Vinit Rathod <vinitrathod100901@gmail.com>
2022-06-09 10:10:52 +0530

    commit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -81,23 +81,26 @@ class Welcome extends CI_Controller
 	{
 		$data['empID'] = $this->emp->getSingleEmployee($id);
 
-		$this->load->view('edit_emp', $data);
-	public function addExpCat() {
-		if($this->input->post('submit')) {
+		$this->load->view('em_edit', $data);
+	}
+	public function addExpCat()
+	{
+		if ($this->input->post('submit')) {
 			$data = array(
 				'c_category' => $this->input->post('expCat'),
 				'c_type' => $this->input->post('expType'),
 				'c_description' => $this->input->post('expDesc')
 			);
 			$insert = $this->exp->insert($data);
-			if($insert) {
+			if ($insert) {
 				redirect('expGetAll');
 			}
 		}
 	}
 
-	public function expGetAll() {
+	public function expGetAll()
+	{
 		$data['exp_details'] = $this->exp->getAll();
-		$this->load->view('exp_views',$data);
+		$this->load->view('exp_views', $data);
 	}
 }
