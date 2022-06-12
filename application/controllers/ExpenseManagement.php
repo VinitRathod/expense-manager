056c72f2d92f40f951c655374339751cd340804b
DESKTOP-M4QI8VT\HENIL <henilmistry74496@gmail.com>
2022-06-12 23:23:56 +0530

    Updated Edit Expense Feature made it 2.0

--- application/controllers/ExpenseManagement.php
+++ application/controllers/ExpenseManagement.php
@@ -38,7 +38,7 @@ class ExpenseManagement extends CI_Controller
 				<td>' . $exps->c_category . '</td>
 				<td>' . $exps->c_description . '</td>
 				<td>' . $exps->c_type . '</td>
-				<td><a href="' . base_url() . 'ExpenseManagement/editExp/' . $exps->c_expid . ' " class="btn btn-success">Edit</a>
+				<td><a href="#" class="btn btn-success" data-toggle="modal" data-target="#editEXPModal" onclick="expEdit(' . $exps->c_expid . ')">Edit</a>
 					<a href="#" class="btn btn-danger" onclick="expDelete(' . $exps->c_expid . ')">Delete</a>
 				</td>
 			</tr>';
@@ -70,7 +70,71 @@ class ExpenseManagement extends CI_Controller
 	public function edit_Exp($id)
 	{
 		$data['exp_details'] = $this->exp->getSingleExp($id);
-		$this->load->view('edit_Exp', $data);
+		$response = $data['exp_details'];
+		// $selectGroup = '<div class="form-group">
+		// 				<label for="exptype"> Select Expense Type :</label>
+		// 				<select id="expType" name="expType">
+		// 					<option value="vendor">Vendor</option>
+		// 					<option value="employee" selected>Employee</option>
+		// 				</select>
+		// 				<span id="warnExpType" class="text-danger font-weight-regular"> </span>
+		// 			</div>';
+		// if($response->c_type == "vendor") {
+		// 	$selectGroup = '<div class="form-group">
+		// 				<label for="exptype"> Select Expense Type :</label>
+		// 				<select id="expType" name="expType">
+		// 					<option value="vendor" selected>Vendor</option>
+		// 					<option value="employee">Employee</option>
+		// 				</select>
+		// 				<span id="warnExpType" class="text-danger font-weight-regular"> </span>
+		// 			</div>';
+		// }
+		// $output = '<div class="modal-dialog">
+		// 				<div class="modal-content">
+
+		// 					<div class="modal-header">
+		// 						<h5 class="modal-title" id="exampleModalLabel">Edit Expense</h5>
+		// 						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		// 							<span aria-hidden="true">&times;</span>
+		// 						</button>
+		// 					</div>
+
+		// 					<div class="modal-body">
+
+		// 						<form action="'.base_url()."ExpenseManagement/expUpdate/".$response->c_expid.'" onsubmit="return validation()" id="add_exp" class="bg-light" method="post">
+		// 							<div class="form-group">
+		// 								<label for="expid" class="font-weight-regular"> Expense Code </label>
+		// 								<input type="text" name="expCode" class="form-control" id="expCode" autocomplete="off" required value="' . $response->c_expcode . '"/>
+		// 								<span id="warnExpCode" class="text-danger font-weight-regular">
+
+		// 								</span>
+		// 							</div>
+
+		// 							<div class="form-group">
+		// 								<label for="expcode" class="font-weight-regular">
+		// 									Expense Category
+		// 								</label>
+		// 								<input type="text" name="expCat" pattern="[a-z A-Z0-9]{1,}" class="form-control" id="expCat" autocomplete="off" required value="' . $response->c_category . '" />
+		// 								<span id="warnFExpCat" class="text-danger font-weight-regular">
+
+		// 								</span>
+		// 							</div>
+									
+		// 							<div class="form-group">
+		// 								<label for="expdesc" class="font-weight-regular"> Expense Description </label>
+		// 								<br />
+		// 								<textarea id="expDesc" rows="4" cols="50" name="expDesc" required>' . $response->c_description . '</textarea>
+		// 							</div>
+
+		// 							' . $selectGroup . '
+
+		// 							<input type="submit" name="submit" value="Submit" class="btn btn-primary" autocomplete="off" data-tw-dismiss="modal" />
+		// 							<input type="reset" name="reset" value="Reset" class="btn btn-secondary" autocomplete="off" />
+		// 						</form>
+		// 					</div>
+		// 				</div>
+		// 			</div>';
+		echo json_encode($response);
 	}
 
 	public function expDelete($id)
@@ -83,7 +147,7 @@ class ExpenseManagement extends CI_Controller
 
 	public function expUpdate($id)
 	{
-		if ($this->input->post("submit")) {
+		// if ($this->input->post("submit")) {
 			$data = array(
 				'c_expcode' => $this->input->post('expCode'),
 				'c_category' => $this->input->post('expCat'),
@@ -94,7 +158,7 @@ class ExpenseManagement extends CI_Controller
 			if ($update) {
 				redirect('ExpenseManagement/expManagement');
 			}
-		}
+		// }
 	}
 
 	// expanse management code ends here =================================================
