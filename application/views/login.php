504fa182d300c270a7b81301cb335fe79d06e2dc
Meet Shroff <meetshroff1762@gmail.com>
2022-06-15 11:49:59 +0530

    login page created

--- application/views/login.php
+++ application/views/login.php
@@ -6,22 +6,21 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Expense Management-Login</title>
   <link href="https://fonts.googleapis.com/css2?family=Muli:wght@400;700&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/style-with-prefix.css" />
-  <style>
-    .srouce {
-      text-align: center;
-      color: #ffffff;
-      padding: 10px;
-    }
-  </style>
-</head>
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/style-side.css" />
+ </head>
 
 <body>
   <div class="container-x">
-    <div class="form-side"></div>
+    <!-- <div class="form-side">
+      <h5>WELCOME TO</h5>
+      <h1>Expense Management</h1>
+    </div> -->
 
     <div class="main-container">
       <div class="form-container">
+        <h4 class="title-xy"> WELCOME TO </h4>
+      <h1 class="title-x"><b> Expense Management </b></h1>
+
         <div class="form-body">
           <h2 class="title"><b> LOG IN </b></h2>
 
