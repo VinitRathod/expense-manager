60dd25d11581d660f40650beb2bc8317301b7fb0
DESKTOP-M4QI8VT\HENIL <henilmistry74496@gmail.com>
2022-06-16 12:35:12 +0530

    added insert-user function in user model

--- application/models/User.php
+++ application/models/User.php
@@ -4,9 +4,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class User extends CI_Model
 {
     private $tbl = "t_users";
-    
+
     public function getAll() {
         return $this->db->get($this->tbl)->result();
+    }
+
+    public function insertUser($data) {
+        return $this->db->insert($this->tbl,$data);
     }
 }
 ?>
