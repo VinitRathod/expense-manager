8d79b91a86d31ad834a30dbbae2390db17d74a23
DESKTOP-M4QI8VT\HENIL <henilmistry74496@gmail.com>
2022-06-20 11:55:06 +0530

    Added some style changes

--- application/views/header.php
+++ application/views/header.php
@@ -25,7 +25,11 @@
 	<!-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	<script src="<?php echo base_url(); ?>assets/sidebar.js"></script>
-
+	<style>
+		.invisible {
+			display: none;
+		}
+	</style>
 
 </head>
 
